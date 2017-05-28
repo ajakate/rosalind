91f2b1ea8b6e4a0cfd9378795221c2c9bd758dc4
ajakate <ajakate@thoughtworks.com>
2017-05-28 18:37:35 -0400

    [#1] refactor find nucleotide method to be more consise

--- src/rosalind/core.clj
+++ src/rosalind/core.clj
@@ -3,7 +3,7 @@
 (defn count-nucleotides
   [dna-sequence]
   (let [frequency-map (frequencies dna-sequence)]
-    [ (get frequency-map \A)
-      (get frequency-map \C)
-      (get frequency-map \G)
-      (get frequency-map \T)]))
+    (map
+      (fn [nucleotide]
+        (get frequency-map nucleotide))
+      [\A \C \G \T])))
